f3498bef9a890b4bcbe03c8c82db35f1e06df5a7
priyanka1226 <96176674+priyanka1226@users.noreply.github.com>
2023-07-25 13:09:32 +0530

    Update settings.py

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -89,7 +89,7 @@ DATABASES={
       'ENGINE':'django.db.backends.postgresql_psycopg2',
       'NAME':'postgres',
       'USER':'postgres',
-      'PASSWORD':'Lohind@77',
+      'PASSWORD':'Password123',
       'HOST':'localhost',
       'PORT':'5432',
    }
